13446dcad9bd4adc21549f93acdd3ee44cd92cd6
Christophe Benz <christophe.benz@nomics.world>
2024-07-01 17:07:30 +0200

    Move local dir out of src

--- local/dev.py
+++ local/dev.py
@@ -8,22 +8,23 @@ vacancies = 15 * np.exp(-0.3 * unemployment_rate)  # Fonction exponentielle déc
 # Créer le graphique avec Plotly
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(
-    x=unemployment_rate,
-    y=vacancies,
-    mode='lines',
-    line=dict(color='blue', width=2),
-    name='Courbe de Beveridge'
-))
+fig.add_trace(
+    go.Scatter(
+        x=unemployment_rate,
+        y=vacancies,
+        mode="lines",
+        line=dict(color="blue", width=2),
+        name="Courbe de Beveridge",
+    )
+)
 
 # Ajouter des titres et des labels
 fig.update_layout(
-    title='Beveridge Curve',
-    xaxis_title='Unemployment rate (%)',
-    yaxis_title='Job vacancy rate (en milliers)',
-    template='plotly_white'
+    title="Beveridge Curve",
+    xaxis_title="Unemployment rate (%)",
+    yaxis_title="Job vacancy rate (en milliers)",
+    template="plotly_white",
 )
 
 # Afficher la courbe
 fig.show()
-
