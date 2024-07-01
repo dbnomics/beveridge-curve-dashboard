df8d467a6af09b96037be5aee6461348dfc26be4
Christophe Benz <christophe.benz@nomics.world>
2024-07-01 17:08:28 +0200

    Reformat files

--- src/beveridge_curve/app.py
+++ src/beveridge_curve/app.py
@@ -1,34 +1,38 @@
-import streamlit as st
-import data_loader as dl
-import charts_creator as cc
 import importlib.resources
+
+import streamlit as st
 from PIL import Image
+
+from . import charts_creator as cc
+from . import data_loader as dl
 
 
 def main():
     package_dir = importlib.resources.files("beveridge_curve")
     st.set_page_config(
         page_title="DBnomics Beveridge Curve",
-        page_icon=str(package_dir / "images/favicon.png")
+        page_icon=str(package_dir / "images/favicon.png"),
     )
     st.image(str(package_dir / "images/dbnomics.svg"), width=300)
-    st.title(':blue[Beveridge  Curve]')
+    st.title(":blue[Beveridge  Curve]")
 
-    tab1, tab2 = st.tabs(['Explanations', "Charts"])
+    tab1, tab2 = st.tabs(["Explanations", "Charts"])
     with tab1:
         st.subheader(":blue[**What is Beveridge's Curve?**]")
         st.write(
             "\n"
             "The Beveridge curve is named after the publications of William Beveridge (1879-1963).\n"
-            " He was an English economist specializing in unemployment, prices, and wages. In 1944, his essay titled \"*Full Employment in a Free Society*\" was published.\n" 
+            ' He was an English economist specializing in unemployment, prices, and wages. In 1944, his essay titled "*Full Employment in a Free Society*" was published.\n'
             "In this essay, Beveridge describes the negative relationship between the job vacancy rate (the job advertisements by companies) and the unemployment rate.\n"
             "\n"
         )
         beveridge_example = Image.open(package_dir / "images/beveridge_example.png")
-        st.image(beveridge_example,
-                 caption='Beveridge Curve in theory',
-                 use_column_width=True, output_format='PNG'
-                 )
+        st.image(
+            beveridge_example,
+            caption="Beveridge Curve in theory",
+            use_column_width=True,
+            output_format="PNG",
+        )
         st.write(
             "\n"
             "\n"
@@ -36,12 +40,12 @@ def main():
             "**What is its usefulness for economists and policy choices?**\n"
             "\n"
             "\n"
-            "For economists, it allows the analysis of \"frictional\" unemployment.\n"
+            'For economists, it allows the analysis of "frictional" unemployment.\n'
             "This is short-term unemployment that occurs during the period needed to find a new job.\n"
             "It is different from cyclical unemployment (which occurs during a recession) and structural unemployment (due to structural changes).\n"
             "\n"
             "For a long time, the Walrasian model (neoclassical model) was the paradigm: the labor market was perfect.\n"
-            "Thus, \"frictional\" unemployment could not exist: a vacant job was immediately filled by a new worker.\n"
+            'Thus, "frictional" unemployment could not exist: a vacant job was immediately filled by a new worker.\n'
             "However, unemployment is present and even very significant in our contemporary economies.\n"
             "Frictional unemployment, therefore, explains a more or less significant part of unemployment.\n"
             "\n"
@@ -61,23 +65,31 @@ def main():
             "Thus, it is not good for an economy to have both a high unemployment rate and a high number of job vacancies simultaneously.\n"
             "This means that even when many positions are available and the workforce is available, unemployment remains high.\n"
             "\n"
-            "An expression has emerged: the Beveridge curve has become \"drunk\" in many countries in recent years.\n"
+            'An expression has emerged: the Beveridge curve has become "drunk" in many countries in recent years.\n'
             "This is due to the explosion of involuntary unemployment. Indeed, in the United States, it can be observed that the Beveridge curve has changed shape over the last decade.\n"
-
         )
 
-
     with tab2:
-        country = st.selectbox('Select a country', ['France', 'United States', 'Germany', 'Euro Area'])
+        country = st.selectbox(
+            "Select a country", ["France", "United States", "Germany", "Euro Area"]
+        )
 
         # Initialiser les données pour obtenir la plage de dates complète
         df_fr, df_us, df_ger, df_eu = dl.prepare_data()
 
         # Trouver les dates minimum et maximum parmi les DataFrames
-        min_date = min(df_fr['original_period'].min(), df_us['original_period'].min(), df_ger['original_period'].min(),
-                       df_eu['original_period'].min())
-        max_date = max(df_fr['original_period'].max(), df_us['original_period'].max(), df_ger['original_period'].max(),
-                       df_eu['original_period'].max())
+        min_date = min(
+            df_fr["original_period"].min(),
+            df_us["original_period"].min(),
+            df_ger["original_period"].min(),
+            df_eu["original_period"].min(),
+        )
+        max_date = max(
+            df_fr["original_period"].max(),
+            df_us["original_period"].max(),
+            df_ger["original_period"].max(),
+            df_eu["original_period"].max(),
+        )
 
         # Sélection des dates avec un slider
         start_date, end_date = st.slider(
@@ -85,30 +97,32 @@ def main():
             min_value=min_date.to_pydatetime(),
             max_value=max_date.to_pydatetime(),
             value=(min_date.to_pydatetime(), max_date.to_pydatetime()),
-            format="YYYY-MM"
+            format="YYYY-MM",
         )
 
         if st.button("Enter"):
-            df_fr, df_us, df_ger, df_eu = dl.prepare_data(start_date=start_date, end_date=end_date)
+            df_fr, df_us, df_ger, df_eu = dl.prepare_data(
+                start_date=start_date, end_date=end_date
+            )
 
             # Afficher le graphique correspondant
-            if country == 'France':
-                st.subheader('Beveridge curve for France')
-                fig = cc.plot_beveridge_curve(df_fr, 'France')
+            if country == "France":
+                st.subheader("Beveridge curve for France")
+                fig = cc.plot_beveridge_curve(df_fr, "France")
                 st.plotly_chart(fig)
-            elif country == 'United States':
-                st.subheader('Beveridge curve for the United States')
-                fig = cc.plot_beveridge_curve(df_us, 'United States')
+            elif country == "United States":
+                st.subheader("Beveridge curve for the United States")
+                fig = cc.plot_beveridge_curve(df_us, "United States")
                 st.plotly_chart(fig)
-            elif country == 'Germany':
-                st.subheader('Beveridge curve for Germany')
-                fig = cc.plot_beveridge_curve(df_ger, 'Germany')
+            elif country == "Germany":
+                st.subheader("Beveridge curve for Germany")
+                fig = cc.plot_beveridge_curve(df_ger, "Germany")
                 st.plotly_chart(fig)
-            elif country == 'Euro Area':
-                st.subheader('Beveridge curve for the Euro Area')
-                fig = cc.plot_beveridge_curve(df_eu, 'Euro Area')
+            elif country == "Euro Area":
+                st.subheader("Beveridge curve for the Euro Area")
+                fig = cc.plot_beveridge_curve(df_eu, "Euro Area")
                 st.plotly_chart(fig)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/beveridge_curve/charts_creator.py
+++ src/beveridge_curve/charts_creator.py
@@ -1,11 +1,12 @@
-# charts_creator.py
 import plotly.express as px
 
 
 def plot_beveridge_curve(df, country):
     # Create a customdata array with the date, unemployment rate, and job vacancy rate
-    df['date'] = df['original_period'].dt.strftime('%Y-%m')
-    df['customdata'] = df.apply(lambda row: [row['date'], row['unemployment rate'], row['job vacancy']], axis=1)
+    df["date"] = df["original_period"].dt.strftime("%Y-%m")
+    df["customdata"] = df.apply(
+        lambda row: [row["date"], row["unemployment rate"], row["job vacancy"]], axis=1
+    )
 
     # Create a scatter plot
     fig = px.scatter(
@@ -17,7 +18,7 @@ def plot_beveridge_curve(df, country):
             "job vacancy": "Job Vacancy rate (%)",
             "unemployment rate": "Unemployment rate (%)",
         },
-        custom_data=['date', 'unemployment rate', 'job vacancy']
+        custom_data=["date", "unemployment rate", "job vacancy"],
     )
 
     # Add a line trace
@@ -27,10 +28,12 @@ def plot_beveridge_curve(df, country):
 
     # Update hover template to include the custom data
     fig.update_traces(
-        hovertemplate="<br>".join([
-            "Date: %{customdata[0]}",
-            "Unemployment rate (%): %{customdata[1]}",
-            "Job Vacancy rate (%): %{customdata[2]}"
-        ])
+        hovertemplate="<br>".join(
+            [
+                "Date: %{customdata[0]}",
+                "Unemployment rate (%): %{customdata[1]}",
+                "Job Vacancy rate (%): %{customdata[2]}",
+            ]
+        )
     )
     return fig
